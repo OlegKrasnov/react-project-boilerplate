28947e1c7f5d5fb3b44a14b6d9fdcfb73d800a66
Oleg Krasnov <oleg.v.krasnov@gmail.com>
2021-11-14 00:03:19 +0300

    Adding autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -82,6 +82,7 @@ module.exports = (env, argv) => {
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
+            'postcss-loader',
             'sass-loader'
           ]
         },
