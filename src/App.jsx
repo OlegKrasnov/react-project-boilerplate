52722b60e7155e1355548090900f473d6d9bb1d1
Oleg Krasnov <oleg.v.krasnov@gmail.com>
2022-04-02 00:40:12 +0300

    IE11 unsupported

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { isIE } from 'react-device-detect'
 import { useBrowserDetect } from './hooks'
 import Footer from '@components/Footer'
 import Header from '@components/Header'
@@ -9,21 +8,15 @@ import Page from '@components/Page'
 const App = () => {
   useBrowserDetect()
 
-  const pageContent =
-    (<div className="container">
-      <Page />
-    </div>)
-
   return (
     <Router>
-      <Header />
-      {
-        isIE ? pageContent :
-          <main>
-            {pageContent}
-          </main>
-      }
-      <Footer />
+      <Header/>
+      <main>
+        <div className="container">
+          <Page/>
+        </div>
+      </main>
+      <Footer/>
     </Router>
   )
 }
