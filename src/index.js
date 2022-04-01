deb1f6a6f4ec4734d7513fa25328db8a21c660b1
Oleg Krasnov <oleg.v.krasnov@gmail.com>
2022-04-01 12:39:56 +0300

    Upgrading to React 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import * as ReactDOMClient from 'react-dom/client'
 import App from './App'
 
 import './assets/styles/main.scss'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root')
+const root = ReactDOMClient.createRoot(rootElement)
+root.render(<App/>)
